e12cc90895ceb68f085154074d9177b3ea476e53
kaizher426 <74589352+kaizher426@users.noreply.github.com>
2021-11-20 11:09:43 +0800

    Update arduino1.ino

--- arduino1.ino
+++ arduino1.ino
@@ -11,6 +11,7 @@ void setup() {
 
 void loop() {
   int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temp: ")
   Serial.println(DHT.temperature);
   Serial.print("Humidity: ");
   Serial.println(DHT.humidity);
