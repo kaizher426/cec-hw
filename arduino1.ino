4ca5124846be4ad5cb00c4501a9a92f1a1827403
kaizher426 <74589352+kaizher426@users.noreply.github.com>
2021-11-20 11:09:02 +0800

    Update arduino1.ino

--- arduino1.ino
+++ arduino1.ino
@@ -9,21 +9,6 @@ void setup() {
 
 }
 
-
-void loop(){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration*0.034/2;
-
-  Serial.print("Distance: ");
-  Serial.println(distance);
-}
-
 void loop() {
   int chk = DHT.read11(DHT11_PIN);
   Serial.println(DHT.temperature);
