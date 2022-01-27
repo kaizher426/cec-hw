568ba5cc7e4bc686825abdf08c85cce80f9ec98c
kaizher426 <74589352+kaizher426@users.noreply.github.com>
2022-01-27 21:59:57 +0800

    Update oldeRuler.ino

--- oldeRuler.ino
+++ oldeRuler.ino
@@ -4,18 +4,20 @@
 
 #define SCREEN_WIDTH 128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
 #define BUTTON_PIN 6
+
 
 // declare an SSD1306 display object connected to I2C
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 int trigPin = 9;    // TRIG pin
 int echoPin = 10;    // ECHO pin
+float PrevValue;
 float duration_us, distance_cm;
-float value;
 
 void setup() {
-  Serial.begin(9600);
+
   // initialize OLED display with address 0x3C for 128x64
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -28,8 +30,6 @@ void setup() {
 }
 
 void loop() {
-  int buttonState;
-  buttonState = digitalRead(BUTTON_PIN);
   // generate 10-microsecond pulse to TRIG pin
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -39,22 +39,25 @@ void loop() {
   duration_us = pulseIn(echoPin, HIGH);
 
   // calculate the distance
-  distance_cm = 0.017 * duration_us;
+ distance_cm = 0.017 * duration_us;
+  
 
-  oled.clearDisplay(); // clear display
-  oled.setTextSize(2);          // text size
+ oled.clearDisplay(); // clear display
+
+int buttonState;
+buttonState = digitalRead (BUTTON_PIN);
+
+ oled.setTextSize(2);          // text size
   oled.setTextColor(WHITE);
   oled.setCursor(0, 0);       // position to display
   oled.println("Distance:");// text color
-  oled.setCursor(0, 30);       // position to display
+  oled.setCursor(0, 20);       // position to display
+  oled.setTextSize(2);  
   oled.println(distance_cm); // text to display
-  if(buttonState = LOW){
-    value = distance_cm;
-    Serial.println("HELLO");
-    }
-   oled.setCursor(0,45);
-   oled.println(value);
-   oled.display();
+if(buttonState==LOW){
+PrevValue = distance_cm;
+}
+oled.println(PrevValue);
+  oled.display();
 
-  
-  }
+}
